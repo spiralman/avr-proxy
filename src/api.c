4baa4138f692e9f77b082c46567409d228823e29
Thomas Stephens <spiralman@gmail.com>
2016-05-14 21:15:02 -0500

    Work around limitations of libesphttpd to capture and send command response; begin work to manage connection state

--- src/api.c
+++ src/api.c
@@ -14,6 +14,14 @@
 static struct espconn avrConn;
 static esp_tcp avrTcp;
 
+static bool avrConnected = false;
+static int avrConnectTime = 0;
+
+static os_timer_t responseTimer;
+static bool awaitingResponse = false;
+static bool responseReady = false;
+static char responseBuf[256];
+
 int ICACHE_FLASH_ATTR
 ssdp_dev_template(HttpdConnData * connData, char * token, void ** arg) {
   if (token == NULL) { return HTTPD_CGI_DONE; }
@@ -23,48 +31,92 @@ ssdp_dev_template(HttpdConnData * connData, char * token, void ** arg) {
   return HTTPD_CGI_DONE;
 }
 
+void ICACHE_FLASH_ATTR response_done_cb(void * arg) {
+  awaitingResponse = false;
+  responseReady = true;
+  os_timer_disarm(&responseTimer);
+}
+
 void ICACHE_FLASH_ATTR cmd_sent_cb(void * arg) {
   os_printf("sent\n");
 }
 
 void ICACHE_FLASH_ATTR cmd_received_cb(void * arg, char * data, unsigned short len) {
+  data[len-1] = '\n';
   os_printf("recieved %s\n", data);
+  if (awaitingResponse) {
+    strcat(responseBuf, data);
+  }
 }
 
 void ICACHE_FLASH_ATTR cmd_connected_cb(void * arg) {
   os_printf("connected\n");
+  avrConnected = true;
+  avrConnectTime = system_get_time();
+}
+
+void ICACHE_FLASH_ATTR cmd_disconnected_cb(void * arg) {
+  os_printf("disconnected\n");
+  avrConnected = false;
+}
+
+void ICACHE_FLASH_ATTR cmd_reconnect_cb(void * arg, sint8 err) {
+  os_printf("reconnect: %d\n", err);
+
 }
 
 int ICACHE_FLASH_ATTR send_command(HttpdConnData * connData) {
-  /* Documented max command length */
-  char cmd[135];
-  int cmdLen;
+  if (!awaitingResponse && !responseReady) {
+    /* Documented max command length */
+    char cmd[135];
+    int cmdLen;
 
-  if (connData->conn==NULL) {
-    //Connection aborted. Clean up.
-    return HTTPD_CGI_DONE;
-  }
+    if (connData->conn==NULL) {
+      //Connection aborted. Clean up.
+      return HTTPD_CGI_DONE;
+    }
 
-  if (connData->requestType != HTTPD_METHOD_GET) {
-    httpdStartResponse(connData, 405);
+    if (connData->requestType != HTTPD_METHOD_GET) {
+      httpdStartResponse(connData, 405);
+      httpdEndHeaders(connData);
+      return HTTPD_CGI_DONE;
+    }
+
+    cmdLen = httpdFindArg(connData->getArgs, "cmd", cmd, sizeof(cmd));
+    if (cmdLen == -1) {
+      httpdStartResponse(connData, 400);
+      httpdEndHeaders(connData);
+      return HTTPD_CGI_DONE;
+    }
+
+    cmd[cmdLen++] = '\r';
+    cmd[cmdLen] = 0;
+
+    os_printf("Sending %s\n", cmd);
+
+    espconn_send(&avrConn, (unsigned char *)cmd, cmdLen);
+    os_timer_arm(&responseTimer, 200, false);
+
+    httpdStartResponse(connData, 200);
     httpdEndHeaders(connData);
+
+    awaitingResponse = true;
+    return HTTPD_CGI_MORE;
+  }
+  else if(responseReady) {
+    os_printf("\nresponse:\n%s\n", responseBuf);
+    httpdSend(connData, responseBuf, -1);
+    responseBuf[0] = 0;
+    responseReady = false;
+
+    os_printf("sent\n");
     return HTTPD_CGI_DONE;
   }
-
-  cmdLen = httpdFindArg(connData->getArgs, "cmd", cmd, sizeof(cmd));
-  if (cmdLen == -1) {
-    httpdStartResponse(connData, 400);
-    httpdEndHeaders(connData);
-    return HTTPD_CGI_DONE;
+  else {
+    os_printf(".");
+    httpdSend(connData, "\n", 1);
+    return HTTPD_CGI_MORE;
   }
-
-  cmd[cmdLen++] = '\r';
-  cmd[cmdLen] = 0;
-
-  os_printf("Sending %s\n", cmd);
-
-  espconn_send(&avrConn, (unsigned char *)cmd, cmdLen);
-  return HTTPD_CGI_DONE;
 }
 
 int ICACHE_FLASH_ATTR hello_world(HttpdConnData * connData) {
@@ -96,6 +148,7 @@ HttpdBuiltInUrl builtInUrls[]={
 
 void ICACHE_FLASH_ATTR api_wifi_status_cb(System_Event_t * evt) {
   if (evt->event == EVENT_STAMODE_GOT_IP) {
+    os_printf("connecting\n");
     espconn_connect(&avrConn);
   }
 }
@@ -103,6 +156,10 @@ void ICACHE_FLASH_ATTR api_wifi_status_cb(System_Event_t * evt) {
 void ICACHE_FLASH_ATTR api_init() {
   espFsInit((void*)(webpages_espfs_start));
   httpdInit(builtInUrls, 80);
+
+  os_timer_setfn(&responseTimer,
+                 response_done_cb,
+                 NULL);
 
   /* TODO: open and close connection dynamically, so we're not sitting
      on it while idle */
@@ -121,6 +178,8 @@ void ICACHE_FLASH_ATTR api_init() {
   espconn_regist_recvcb(&avrConn, cmd_received_cb);
   espconn_regist_sentcb(&avrConn, cmd_sent_cb);
   espconn_regist_connectcb(&avrConn, cmd_connected_cb);
+  espconn_regist_disconcb(&avrConn, cmd_disconnected_cb);
+  espconn_regist_reconcb(&avrConn, cmd_reconnect_cb);
 
   wifi_set_event_handler_cb(api_wifi_status_cb);
 };
